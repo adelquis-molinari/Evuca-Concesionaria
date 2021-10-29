b32a183a7135ef6e64a8f193cf6e01a2626a5174
JoaquinMPereyra <joaquinmpereyra@hotmail.com>
2021-10-29 06:14:44 -0300

    Base del home, agregue algo al package json y puse react router

--- src/App.js
+++ src/App.js
@@ -1,27 +1,16 @@
 import logo from './logo.svg';
+import React from 'react'
 import Nav from './conponents/NavBar/Navbar'
+import Home from "./conponents/Home/Home.js"
+import { Route } from "react-router-dom";
 import './App.css';
 
-function App() {
-  
+export function App() {
   return (
-    <div className="App">
+    <React.Fragment>
       <Nav />
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <Route exact path="/" component={Home}/>
+    </React.Fragment>
   );
 }
 
