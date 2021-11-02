e53a4a5d93776fde43826a6b0e112ef38cc631ae
10maxipiutrin <massipiutrin@gmail.com>
2021-11-02 17:58:38 -0300

    login

--- src/conponents/Logins/Logins.jsx
+++ src/conponents/Logins/Logins.jsx
@@ -1,14 +1,28 @@
 import React from 'react'
+import "./login.css"
 
 const Logins = () => {
     return ( 
-        <div>
-            
-            <br />
-            <br />
-            <br /><br />
-            <h1>Logins</h1>
-        </div>
+        <div className= "login-box">
+        <img className= "avatar" src="logo png azul.png"/>
+        <h1>Iniciar Sesion</h1>
+        <form>
+            <label >Correo Electrónico</label>
+            <input type="text" placeholder="Enter Username"/>
+
+            <label for="password">Contraseña</label>
+            <input type="password" placeholder="Enter password"/>
+
+            <input type="Submit" value="Log In"/>
+
+            <a href="#">olvidaste tu contraseña?</a><br/>
+            <a href="#">No tienes una cuenta?</a>
+        </form>
+
+
+
+
+    </div>
      );
 }
  
