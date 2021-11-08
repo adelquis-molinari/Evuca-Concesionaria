a31c49db2e40b629c588ddde41f08d644563fc77
adelquis-molinari <66643930+adelquis-molinari@users.noreply.github.com>
2021-11-08 19:14:06 -0300

    cambios en api

--- src/Data/apiAutos.js
+++ src/Data/apiAutos.js
@@ -1,35 +1,35 @@
 import ford from '../img/autos/ford/ford.png';
-import mondeo from '../img/autos/ford/mondeo.jpg';
-import bronco from '../img/autos/ford/bronco.jpg';
-import ecosport from '../img/autos/ford/ecosport.jpg';
-import territory from '../img/autos/ford/territory.jpg';
+import mondeo from '../img/autos/ford/mondeo/mondeo.jpg';
+import bronco from '../img/autos/ford/bronco/bronco.jpg';
+import ecosport from '../img/autos/ford/ecosport/ecosport.jpg';
+import territory from '../img/autos/ford/territory/territory.jpg';
 import fiat from '../img/autos/fiat/fiat.png';
-import mobi from '../img/autos/fiat/mobi.png';
-import toro from '../img/autos/fiat/toro.png';
-import uno from '../img/autos/fiat/uno.png';
+import mobi from '../img/autos/fiat/mobi/mobi.png';
+import toro from '../img/autos/fiat/toro/toro.png';
+import uno from '../img/autos/fiat/uno/uno.png';
 import peugeot from '../img/autos/peugeot/peugeot.png';
-import p208 from '../img/autos/peugeot/208.png';
-import p308 from '../img/autos/peugeot/308.png';
-import p5008 from '../img/autos/peugeot/5008.png';
-import patagonica from '../img/autos/peugeot/patagonica.png';
+import p208 from '../img/autos/peugeot/208/208.png';
+import p308 from '../img/autos/peugeot/308/308.png';
+import p5008 from '../img/autos/peugeot/5008/5008.png';
+import patagonica from '../img/autos/peugeot/patagonica/patagonica.png';
 import renault from '../img/autos/renault/renault.png';
-import captur from '../img/autos/renault/captur.jpg';
-import logan from '../img/autos/renault/logan.jpg';
-import sandero from '../img/autos/renault/sandero.jpg';
-import steway from '../img/autos/renault/steway.jpg';
+import captur from '../img/autos/renault/captur/captur.jpg';
+import logan from '../img/autos/renault/logan/logan.jpg';
+import sandero from '../img/autos/renault/sandero/sandero.jpg';
+import steway from '../img/autos/renault/steway/steway.jpg';
 import volkswagen from '../img/autos/volkswagen/volkswagen.png';
-import gol from '../img/autos/volkswagen/gol.png';
-import polo from '../img/autos/volkswagen/polo.png';
-import virtus from '../img/autos/volkswagen/virtus.png';
+import gol from '../img/autos/volkswagen/gol/gol.png';
+import polo from '../img/autos/volkswagen/polo/polo.png';
+import virtus from '../img/autos/volkswagen/virtus/virtus.png';
 
 export const apiSimpleAutos= [
     {
         id: 179760,
         marca: 'Ford',
-        modelo: 'Mondeo',
+        modelo: 'Mondeo Híbrido',
         img: mondeo,
         imgMarca: ford,
-        descripcion: 'Ford Mondeo 2021'
+        descripcion: 'Ford Mondeo Híbrido 2021'
     },{
         id: 255882,
         marca: 'Ford',
@@ -149,5 +149,185 @@ export const apiSimpleAutos= [
         img: virtus,
         imgMarca: volkswagen,
         descripcion: 'Volkswagen Virtus 2021'
+    }
+]
+export const apiDetalladaAutos= [
+    {
+        id: 1718423,
+        marca: 'Ford',
+        modelo: 'Mondeo Híbrido',
+        img: mondeo,
+        imgMarca: ford,
+        imgBanner: "bannerMondeo",
+        imgDescriptivas: 0,
+        Titulo: 'Ford Mondeo Eficiencia y rendimiento',
+        descripcion: 'Bajo el capót de un híbrido verán las mismas partes que en un vehículo convencional más una gran diferencia: un motor eléctrico. El motor de combustión interna y el motor eléctrico trabajan en conjunto para propulsar el vehículo.'
+    },{
+        id: 2558843,
+        marca: 'Ford',
+        modelo: 'Bronco',
+        img: bronco,
+        imgMarca: ford,
+        imgBanner: "bannerBronco",
+        imgDescriptivas: 0,
+        Titulo: 'El icono de la aventura',
+        descripcion: 'La Bronco Sport Wildtrak cuenta con un avanzado sistema de tracción 4WD con control vectorial de torque y bloqueo de diferencial trasero. Además, su Sistema de Gestión de Terrenos (GOAT) tiene hasta 7 modos de manejo.'
+    },{
+        id: 4670547,
+        marca: 'Ford',
+        modelo: 'Ecosport',
+        img: ecosport,
+        imgMarca: ford,
+        imgBanner: "bannerEcosport",
+        imgDescriptivas: 0,
+        Titulo: 'Creara para transfornar tu imaginación.',
+        descripcion: 'La Ecosport conserva los elementos que definen su identidad: transmite confianza, combina refinamiento urbano con espíritu aventurero y revela una alta calidad en su dinámica de manejo.'
+    },{
+        id: 8944585,
+        marca: 'Ford',
+        modelo: 'Territory',
+        img: territory,
+        imgMarca: ford,
+        imgBanner: "bannerTerritory",
+        imgDescriptivas: 0,
+        Titulo: 'Entrentemos la rutina, enfrentemos el cambio',
+        descripcion: 'La Territory incorpora Ford Co-Pilot 360, un avanzado sistema de asistencia a la conducción de vanguardia, que incluye camara 360, alerta de puntos ciego, Sistema antibloqueo de frenos (ABS), distribución electrónica de frenado (EBD) y asistencia electrónica al frenado de emergencia (EBA)​'
+    },{
+        id: 5858365,
+        marca: 'Fiat',
+        modelo: 'Mobi',
+        img: mobi,
+        imgMarca: fiat,
+        imgBanner: "bannerMobi",
+        imgDescriptivas: 0,
+        Titulo: 'Nuevo Fiat MOBI Like',
+        descripcion: 'El FIAT Mobi mantiene su esencia pero ahora renueva su parrilla delantera y algunos otros detalles en línea con el resto de los modelos de FIAT. Este city car también agrega novedades en el equipamiento como la central multimedia de 7’’ con conectividad Android Auto & Carplay.'
+    },{
+        id: 3356634,
+        marca: 'Fiat',
+        modelo: 'Toro',
+        img: toro,
+        imgMarca: fiat,
+        imgBanner: "bannerToro",
+        imgDescriptivas: 0,
+        Titulo: 'Tienes todo lo que necesitás para una nueva aventura.',
+        descripcion: 'De una, te va a fascinar el nuevo capot. Después, te vas a enamorar de la nueva parrilla, con el logo de Fiat y el detalle de la bandera italiana. Un diseño musculoso, que equilibra belleza y fuerza. El frente de la nueva Fiat Toro es increible.'
+    },{
+        id: 1732428,
+        marca: 'Fiat',
+        modelo: 'Uno Way',
+        img: uno,
+        imgMarca: fiat,
+        imgBanner: "bannerUno",
+        imgDescriptivas: 0,
+        Titulo: 'Varios autos en UNO solo.',
+        descripcion: 'El Fiat Uno llama la atención. Además de su renovado diseño frontal –más moderno y lleno de personalidad–, el Uno cuenta con nuevos paragolpes y parrilla, que resaltan los contornos del modelo.'
+    },{
+        id: 8926023,
+        marca: 'Peugeot',
+        modelo: '208',
+        img: p208,
+        imgMarca: peugeot,
+        imgBanner: "banner208",
+        imgDescriptivas: 0,
+        Titulo: 'Unboring the future',
+        descripcion: 'El diseño del 208 adopta el nuevo lenguaje de diseño de la marca Peugeot, con la nueva firma lumínica que se asemeja a los colmillos del León. En el frente, aparece el nombre del modelo para un toque distintivo y moderno. En el lateral, el juego de sombras le da un perfil dinámico. Y en la parte posterior, las luces traseras con forma de garra completan un diseño armonioso.'
+    },{
+        id: 34743923,
+        marca: 'Peugeot',
+        modelo: '308',
+        img: p308,
+        imgMarca: peugeot,
+        imgBanner: "banner308",
+        imgDescriptivas: 0,
+        Titulo: 'Estilo y Elegancia',
+        descripcion: 'De nuevas líneas en su diseño exterior, el Peugeot 308 integra los últimos códigos estilísticos de la marca. A simple vista, un estilo moderno con detalles tecnológicos lumínicos impactantes, de frente felino, que conjuga lo dinámico y lo exclusivo.'
+    },{
+        id: 15809567,
+        marca: 'Peugeot',
+        modelo: 'SUV 5008',
+        img: p5008,
+        imgMarca: peugeot,
+        imgBanner: "banner5008",
+        imgDescriptivas: 0,
+        Titulo: 'SUV Peugeot 5008 ',
+        descripcion: 'Dejate seducir por el nuevo SUV PEUGEOT 5008. Con un nuevo frente, este SUV tiene líneas elegantes y modernas que conjugan en un diseño sofisticado. Un vehículo pensado para disfrutar en familia gracias a sus 7 plazas.'
+    },{
+        id: 2347653,
+        marca: 'Peugeot',
+        modelo: 'Partner Patagonica',
+        img: patagonica,
+        imgMarca: peugeot,
+        imgBanner: "bannerPatagonica",
+        imgDescriptivas: 0,
+        Titulo: 'La Compañera para tu familia',
+        descripcion: 'Con su aspecto que inspira robustez y dinamismo, Partner Patagonica afirma su pertenencia al universo de los monovolumenes. Sus faros de vidrio liso y las protecciones reforzadas imponen un carácter robusto de confianza y solidez. '
+    },{
+        id: 7046034,
+        marca: 'Renault',
+        modelo: 'Captur',
+        img: captur,
+        imgMarca: renault,
+        descripcion: 'Renault Captur 2021'
+    },{
+        id: 4963043,
+        marca: 'Renault',
+        modelo: 'Logan',
+        img: logan,
+        imgMarca: renault,
+        imgBanner: "bannerLogan",
+        imgDescriptivas: 0,
+        Titulo: 'Un sedán para el momento en el que estás.',
+        descripcion: 'El diseño renovado, el gran espacio interior junto con un baúl suficientemente grande para todo lo que querés llevar, son algunas de las características que hacen al Renault LOGAN, el vehículo que te merecés.'
+    },{
+        id: 4963355,
+        marca: 'Renault',
+        modelo: 'Sandero',
+        img: sandero,
+        imgMarca: renault,
+        imgBanner: "bannerSandero",
+        imgDescriptivas: 0,
+        Titulo: 'Sos urbano,sos alguien con caractér.',
+        descripcion: 'Con un diseño completamente renovado, su nueva identidad lumínica, llantas diamantadas 16” y su caja CVT, el Renault SANDERO es el auto perfecto que se adapta a todo lo que sos.'
+    },{
+        id: 2447456,
+        marca: 'Renault',
+        modelo: 'Steway',
+        img: steway,
+        imgMarca: renault,
+        imgBanner: "bannerSteway",
+        imgDescriptivas: 0,
+        Titulo: 'Un vehículo con actitud para enfrentar cualquier camino.',
+        descripcion: 'Su gran despeje del suelo y sus nuevos faros C-Shape le otorgan una actitud intrépida que junto a las luces LED traseras, trazan una línea que resalta el movimiento. Un auto diseñado a la altura de lo que te propongas .'
+    },{
+        id: 3957998,
+        marca: 'Volkswagen',
+        modelo: 'Gol',
+        img: gol,
+        imgMarca: volkswagen,
+        imgBanner: "bannerGol",
+        imgDescriptivas: 0,
+        Titulo: 'El auto que te mereces',
+        descripcion: 'El Gol incorpora en su versión Trendline la caja automática Tiptronic de 6 velocidades para que disfrutes de tus viajes como nunca antes. Esta nueva característica se combina con el reconocido motor 1.6 l de 16 V y 110 CV.'
+    },{
+        id: 8925180,
+        marca: 'Volkswagen',
+        modelo: 'Polo',
+        img: polo,
+        imgMarca: volkswagen,
+        imgBanner: "bannerPolo",
+        imgDescriptivas: 0,
+        Titulo: 'Vas a querer llamarlo tu nuevo auto',
+        descripcion: 'El nuevo Polo GTS se convierte en una referencia en deportividad dentro de su segmento gracias a su diseño imponente y exclusivo. Los detalles de calidad en rojo y negro brillante, los faros full LED y su frontal aguerrida nos muestran toda la deportividad en una sola mirada.'
+    },{
+        id: 9197791,
+        marca: 'Volkswagen',
+        modelo: 'Virtus',
+        img: virtus,
+        imgMarca: volkswagen,
+        imgBanner: "bannerVirtus",
+        imgDescriptivas: 0,
+        Titulo: 'La potencia que buscás, con la comodidad que necesitás.',
+        descripcion: 'El Nuevo Virtus GTS se convierte en una nueva referencia en deportividad marcando diferencia con su gran espacio interno. Su diseño imponente exclusivo trae detalles de altísima calidad en color rojo y negro sumados a las líneas cromadas en su exterior. Su frontal aguerrido nos muestra toda la deportividad en una sola mirada.'
     }
 ]
--- src/Data/apiMotos.js
+++ src/Data/apiMotos.js
@@ -9,7 +9,7 @@ import cbr1000 from '../img/motos/honda/cbr1000.jpg';
 import yamaha from '../img/motos/yamaha/yamaha.png';
 import cripton from '../img/motos/yamaha/cripton.jpg';
 import fz25 from '../img/motos/yamaha/fz25.jpg';
-import FZFR1 from '../img/motos/yamaha/FZF-R1.jpg';
+import YZFR1 from '../img/motos/yamaha/FZF-R1.jpg';
 
 export const apiSimpleMotos = [
     {
@@ -72,8 +72,104 @@ export const apiSimpleMotos = [
         id: 603092,
         marca: 'Yamaha',
         modelo: 'FZ FR1',
-        img: FZFR1,
+        img: YZFR1,
         imgMarca: yamaha,
         descripcion: 'yamaha fz fr1 2021'
+    }
+]
+
+export const apiDetalladaMotos = [
+    {
+        id: 3060473,
+        marca: 'Corven',
+        modelo: 'Hunter 15 rt',
+        img: hunter150rt,
+        imgMarca: corven,
+        imgBanner: "bannerVirtus",
+        imgDescriptivas: 0,
+        Titulo: 'Ágil y rendidora.',
+        descripcion: 'Con su renovado diseño, la Hunter 150 RT combina agilidad, rendimiento y prestaciones con una ideal economía de uso. Se presenta con un diseño moderno, deportivo y con detalles de calidad en todos sus componentes. Con baulera incorporada, brinda practicidad y comodidad para todos tus viajes.'
+    },{
+        id: 6279014,
+        marca: 'Corven',
+        modelo: 'New Energy 110',
+        img: newEnergy110,
+        imgMarca: corven,
+        imgBanner: "bannerVirtus",
+        imgDescriptivas: 0,
+        Titulo: 'Ágil y versátil.',
+        descripcion: 'La Energy 110 RT es la mejor opción a la hora de elegir una motocicleta ágil y fácil de conducir, que te aporte la libertad que necesitas para tu movimiento cotidiano.'
+    },{
+        id: 4267337,
+        marca: 'Corven',
+        modelo: 'Touring Triax 250',
+        img: touringTriax250,
+        imgMarca: corven,
+        imgBanner: "bannerVirtus",
+        imgDescriptivas: 0,
+        Titulo: 'Comodidad y movilidad.',
+        descripcion: 'Una moto de diseño exclusivo, que incorpora un completo equipamiento pensado para quienes desean recorrer grandes distancias, deseando vivir un viaje aventurero, pero sobre todo cómodo y seguro.'
+    },{
+        id: 9046973,
+        marca: 'Honda',
+        modelo: 'Wave',
+        img: wave,
+        imgMarca: honda,
+        imgBanner: "bannerVirtus",
+        imgDescriptivas: 0,
+        Titulo: 'El más versátil de los motocicletas.',
+        descripcion: 'Cuenta con una excelente maniobrabilidad gracias a su cuerpo ligero, que sumado a su asiento doble, la convierten en la opción ideal para moverte por la ciudad todos los días.'
+    },{
+        id: 3618087,
+        marca: 'Honda',
+        modelo: 'Twister',
+        img: twister,
+        imgMarca: honda,
+        imgBanner: "bannerVirtus",
+        imgDescriptivas: 0,
+        Titulo: 'La siempre confiable.',
+        descripcion: 'Moderno e innovador, el panel es totalmente digital y te brinda todo lo que necesitás para una visualización óptima en todo momento. Incluye velocímetro, odómetro total y parcial, indicador de nivel de combustible y un reloj. Además, se incorpora la función de autonomía en el tablero (kilómetros por combustible disponible).'
+    },{
+        id: 9473107,
+        marca: 'Honda',
+        modelo: 'CBR 1000',
+        img: cbr1000,
+        imgMarca: honda,
+        imgBanner: "bannerVirtus",
+        imgDescriptivas: 0,
+        Titulo: 'Más liviana, más poderosa.',
+        descripcion: 'Más liviana, más poderosa, más control. La nueva CBR1000RR SP1 tiene un motor de 16 válvulas DOHC con 10 caballos de fuerza más y contribuyendo una disminución en su peso total. Tiene una mejora de 14% en potencia sobre peso. La nueva SP mantiene al usuario amigable, dado a su gran paquete electrónico que es lo principal de su concepto de control total.'
+    },{
+        id: 2286429,
+        marca: 'Yamaha',
+        modelo: 'Cripton',
+        img: cripton,
+        imgMarca: yamaha,
+        imgBanner: "bannerVirtus",
+        imgDescriptivas: 0,
+        Titulo: 'Movilidad urbana.',
+        descripcion: 'Con un andar cómodo y sencillo, es una excelente opción para la movilidad urbana diaria. Posee un motor 4 tiempos que optimiza el consumo y brinda un andar suave y con excelentes prestaciones. Por su estética y dimensiones es un vehículo versátil y fácil de conducir en trayectos cortos y medianos. Además posee un completo tablero, caballete central y lateral así como baúl portaobjetos bajo el asiento.'
+    },{
+        id: 4237819,
+        marca: 'Yamaha',
+        modelo: 'FZ 25',
+        img: fz25,
+        imgMarca: yamaha,
+        imgBanner: "bannerVirtus",
+        imgDescriptivas: 0,
+        Titulo: 'ADN FZ.',
+        descripcion: `Siendo el mayor exponente de la serie, FZ25 combina diseño y rendimiento de una forma única. Sus líneas brindan un carácter urbano desafiante, deportivo y bien diferenciado, mientras que su comodidad y rendimiento le permiten un uso cómodo tanto en ciudad como en ruta. 
+        Diseñado bajo el concepto Blue Core, ofrece gran rendimiento y eficiencia de consumo de combustible, que sumado a su chasis tipo diamante y al escape Mid-ship este modelo es una opción equilibrada y ágil para la conducción urbana.`
+    },{
+        id: 3100762,
+        marca: 'Yamaha',
+        modelo: 'YFZ FR1',
+        img: YZFR1,
+        imgMarca: yamaha,
+        imgBanner: "bannerVirtus",
+        imgDescriptivas: 0,
+        Titulo: 'Espíritu deportivo.',
+        descripcion: `Con la tecnología “CROSSPLANE” derivada de la YZR-M1 que utiliza el equipo Yamaha Factory de MotoGP y un diseño realizado de principio a fin por el icono del motociclismo: Valentino Rossi, llega la nueva YZF-R1 para crear experiencias con sensaciones únicas. 
+        Un referente de la marca que ha evolucionado para mantener su liderazgo tanto en la pista como en las calles.`
     }
 ]
