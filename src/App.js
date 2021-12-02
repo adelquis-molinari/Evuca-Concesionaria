3d88b788bdf8e78812ce22a67aaf215ff916100d
adelquis-molinari <66643930+adelquis-molinari@users.noreply.github.com>
2021-12-02 17:11:37 -0300

    ruta

--- src/App.js
+++ src/App.js
@@ -75,10 +75,10 @@ function App(props) {
         <Route  path="/busqueda" render={() => <Search />}/> 
         <Route  path="/shop/:id" component={Shop}/> 
         <Route  path="/article/:id" component={Articulo}/> 
-        <Route  path="/shopping-cart" render={()=> <ShoppingCart />}/> 
+        <PrivateRoute  path="/shopping-cart" component={<ShoppingCart />}/> 
         <PrivateRoute  path="/contact" component={Contacto}/>
         <PrivateRoute  path="/shift-dashboard" component={ShiftDashboard}/>
-        <Route  path="/admin" render={() => <DashBoard/>}/>
+        <PrivateRoute  path="/admin" component={<DashBoard/>}/>
         {/* <Route component={Pag404}/> */}
       <Footer />
     </Fragment>
